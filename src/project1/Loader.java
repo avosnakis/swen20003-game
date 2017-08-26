98e2e9cfa5c975e3e1db58ca80b0ca22fd16c680
Alex <alex.vosnakis@alexsolutions.com.au>
2017-08-26 13:26:38 +1000

    Added readMapDimensions method

--- src/project1/Loader.java
+++ src/project1/Loader.java
@@ -3,12 +3,16 @@ package project1;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
+
+import static project1.App.TILE_SIZE;
 
 public class Loader {
 
-  /* Every entity has a width and length of 32 pixels*/
-  private static final int ENTITY_WIDTH = 32;
+  private static final int NUM_DIMENSIONS = 2;
+  private static final int X_INDEX = 0;
+  private static final int Y_INDEX = 1;
+
+  private static final char COMMA_SEPARATOR = ',';
 
 	// Converts a world coordinate to a tile coordinate,
 	// and returns if that location is a blocked tile
@@ -22,21 +26,32 @@ public class Loader {
 	 * @param filename
 	 * @return
 	 */
-	public static ArrayList<Sprite> loadSprites(String filename) {
-	  ArrayList<Sprite> sprites = new ArrayList<Sprite>();
+	public static Sprite[] loadSprites(String filename) {
 
 	  try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
-      String text = br.readLine();
+      int[] levelDimensions = readMapDimensions(br.readLine());
+      int xDimension = levelDimensions[X_INDEX];
+      int yDimension = levelDimensions[Y_INDEX];
+
+      String text;
       while ((text = br.readLine()) != null) {
 
       }
     } catch (IOException e) {
 	    e.printStackTrace();
     }
-		return null;
+    return null;
 	}
 
+	/* Find the dimensions of the level we are reading from the first line of the .lvl file*/
 	private static int[] readMapDimensions(String text) {
-	  return null;
+	  int[] levelDimensions = new int[NUM_DIMENSIONS];
+    int separatorPosition = text.indexOf(COMMA_SEPARATOR);
+
+    /* X dimension is the first number */
+    levelDimensions[X_INDEX] = Integer.parseInt(text.substring(0, COMMA_SEPARATOR));
+    /* Y dimension is the second number. We add 1 to end index as it is exclusive */
+    levelDimensions[Y_INDEX] = Integer.parseInt(text.substring(COMMA_SEPARATOR, text.length() + 1));
+	  return levelDimensions;
   }
 }
